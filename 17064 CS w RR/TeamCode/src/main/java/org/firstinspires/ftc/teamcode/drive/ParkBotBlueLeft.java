4e8016a469cdd5c7f8c062affd2cc1bda1c79127
Team 9933 FTC <55024397+9933ftcrobotics@users.noreply.github.com>
2023-11-24 12:03:01 -0500

    "Park Bot" Auto Change

--- 17064 CS w RR/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/ParkBotBlueLeft.java
+++ 17064 CS w RR/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/ParkBotBlueLeft.java
@@ -5,6 +5,9 @@ import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
@@ -12,11 +15,35 @@ import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 @Autonomous(group = "ParkBotBlueLeft")
 
 public class ParkBotBlueLeft extends LinearOpMode {
+    private Servo RightClaw;
+    private Servo LeftClaw;
+    private DcMotor ArmUpDown;
+    private DistanceSensor LeftDistance;
+    private DistanceSensor RightDistance;
+
     @Override
     public void runOpMode() throws InterruptedException {
+
+        RightClaw=hardwareMap.servo.get("RightClaw");
+        LeftClaw=hardwareMap.servo.get("LeftClaw");
+        ArmUpDown=hardwareMap.dcMotor.get("ArmUpDown");
+        LeftDistance = hardwareMap.get(DistanceSensor.class, "LeftDistance");
+        RightDistance = hardwareMap.get(DistanceSensor.class, "RightDistance");
+
+        ArmUpDown.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        ArmUpDown.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        ArmUpDown.setTargetPosition(0);
+        ArmUpDown.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        LeftClaw.setDirection(Servo.Direction.REVERSE);
+
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
         waitForStart();
+
+        RightClaw.setPosition(0.21);
+        LeftClaw.setPosition(0.3);
+        ArmUpDown.setTargetPosition(100);
 
         if (isStopRequested()) return;
         drive.setPoseEstimate(new Pose2d(8, -60, Math.toRadians(90)));
